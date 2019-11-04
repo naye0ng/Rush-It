9de0610b868964a9c8040561a5e0aad4911f09d6
Yongbeom Jo <jybtask@gmail.com>
2019-11-04 11:24:59 +0900

    Add userTest function

--- backend/src/main/java/com/rushit/controller/UserController.java
+++ backend/src/main/java/com/rushit/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.rushit.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +26,11 @@ public class UserController {
 	@Autowired
 	public void setUserService(UserService userService) {
 		this.userService = userService;
+	}
+	
+	@GetMapping("/usertest")
+	public void test(HttpServletResponse response) throws IOException {
+		response.getWriter().print("hello");
 	}
 	
 	@PostMapping("/user")
