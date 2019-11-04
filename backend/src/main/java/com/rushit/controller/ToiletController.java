1eb61f0dfb6aff95d955494c54b5df30c8455ed0
Yongbeom Jo <jybtask@gmail.com>
2019-11-04 11:21:01 +0900

    Add crossOrigin annotation in controller, Delete errorController

--- backend/src/main/java/com/rushit/controller/ToiletController.java
+++ backend/src/main/java/com/rushit/controller/ToiletController.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +25,7 @@ import com.rushit.model.service.UserService;
 import com.rushit.model.vo.Review;
 import com.rushit.model.vo.Toilet;
 
-
+@CrossOrigin(origins= {"*"})
 @RestController
 public class ToiletController {
 	private ToiletService ts;
