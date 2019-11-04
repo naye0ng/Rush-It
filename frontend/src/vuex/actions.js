cf034b0a01eb703e204f6919677f1b143dedebbf
naye0ng <nayeong_e@naver.com>
2019-11-04 16:30:15 +0900

    Add 아이콘 및 이미지 추가

--- frontend/src/vuex/actions.js
+++ frontend/src/vuex/actions.js
@@ -3,11 +3,33 @@ import axios from 'axios'
 const url = "http://13.125.1.123:8080"
 export default {
     asyncSignIn({commit}, payload){
-        if (payload.signIn_id && payload.signIn_pw){
-            console.log("비어있지 않다")
-            // auto login이 true이면 로컬 스토리지에 저장하자
+        let data = JSON.stringify({
+            pw: payload.pw
+        })
+        console.log(data)
+        axios.get(url+'/usertest').then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        // axios.post(url+'/user/'+payload.id, data, {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     }
+        // }).then(function (response) {
+        //     console.log(response);
+        //   })
+        //   .catch(function (error) {
+        //     console.log(error);
+        //   });
+        //로그인처리할 것
+        return false
+        // if (payload.signIn_id && payload.signIn_pw){
+        //     console.log("비어있지 않다")
+        //     // auto login이 true이면 로컬 스토리지에 저장하자
             
-        }
+        // }
     },
     asyncSignUp({commit}, payload){
         if (payload.signUp_id && payload.signUp_nick && payload.signUp_pw && payload.signUp_pw2){
--- frontend/src/vuex/mutations.js
+++ frontend/src/vuex/mutations.js
@@ -20,6 +20,7 @@ export default {
         state.currentPage = payload
     },
     setAuthentication(state, payload){
+        console.log(payload)
         for(var key in payload){
             state.authentication[key] = payload[key]
         }
