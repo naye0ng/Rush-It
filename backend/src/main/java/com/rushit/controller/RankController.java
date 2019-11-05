7a225eee9fea3c2f8c0a1115097246b7fc46eddc
Yongbeom Jo <jybtask@gmail.com>
2019-11-05 12:36:09 +0900

    Add ToiletRankList

--- backend/src/main/java/com/rushit/controller/RankController.java
+++ backend/src/main/java/com/rushit/controller/RankController.java
@@ -1,5 +1,27 @@
 package com.rushit.controller;
 
-public class RankController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rushit.model.service.RankService;
+import com.rushit.model.vo.tRank;
+
+@CrossOrigin(origins= {"*"})
+@RestController
+public class RankController {
+	private RankService rankService; 
+
+	@Autowired
+	public void setRankService(RankService rankService) {
+		this.rankService = rankService;
+	}
+	
+	@GetMapping("/rank/toilet")
+	public List<tRank> toiletRank() {
+		return rankService.getToiletRank();
+	}
 }
